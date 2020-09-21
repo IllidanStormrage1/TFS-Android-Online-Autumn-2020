34228635b2428ac4d283b78af006612c19dbd860
Kirill Zudov <ueurhfyh1@gmail.com>
2020-09-21 09:14:43 +0000

    First HomeWork @lounvh

---
+++ app/src/main/java/com/example/homework150920/ui/receiver/SimpleLocalBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.homework150920.ui.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.homework150920.ui.EXTRA_DATA_NAME
+
+class SimpleLocalBroadcastReceiver(private val action: (Intent) -> Unit) : BroadcastReceiver() {
+    override fun onReceive(content: Context?, intent: Intent) {
+        if (intent.hasExtra(EXTRA_DATA_NAME))
+            action.invoke(intent)
+    }
+}
